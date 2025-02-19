93ae2b68648c113501ccb146d2972129ccf81295
AliZain <alizain.vibes@gmail.com>
2025-02-19 17:45:32 +1100

    Percentage

---
+++ Persentage.py
@@ -0,0 +1,9 @@
+print("Enter your marks in the following subject")
+Maths=int(input("Enter your marks for maths"))
+Drama=int(input("Enter your marks for drama"))
+PE=int(input("Enter your marks for PE"))
+IT=int(input("Enter your marks for IT"))
+sum=Maths+Drama+PE+IT
+print(sum)
+percentage=(sum/400)*100
+print(percentage)
